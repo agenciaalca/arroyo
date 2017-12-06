69f14b894bfe640088c4f7f5e65b0323b2822544
cristiianefaria <cristiane.trc@gmail.com>
2017-12-06 15:28:29 -0200

    estrutura inicial

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content=""/>
+        <meta name="keywords" content=""/>
+        <meta name="author" content="Agência Alca"/>
+
+        <title></title>
+        <link rel="icon" href="img/favicon.png" />
+
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+        
+        
+        
+       
+    </head>
+<body>
+
+
+<main>
