1b9fe172b35e4e08a1fe5d15a83768ba79223485
cristiianefaria <cristiane.trc@gmail.com>
2017-12-06 17:25:50 -0200

    criando estrutura conforme layout

--- header.php
+++ header.php
@@ -13,12 +13,31 @@
         <link rel="stylesheet" href="css/font-awesome.min.css">
         <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
         <link rel="stylesheet" type="text/css" href="css/style.css" />
-        
-        
-        
-       
+
     </head>
-<body>
+    <body>
+        <nav class="navbar navbar-custom navbar-fixed-top">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Brand</a>
+                </div>
 
-
-<main>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#home">HOME</a></li>
+                        <li><a href="#sobre">SOBRE</a></li>
+                        <li><a href="#servicos">SERVIÇOS</a></li>
+                        <li><a href="#planos">PLANOS</a></li>
+                        <li><a href="#contato">CONTATO</a></li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container -->
+        </nav>
--- index.php
+++ index.php
@@ -2,11 +2,175 @@
 <?php include 'mail.php'; ?>
 
 
-<nav></nav>
-<header></header>
-<section class="">Conteudo 1</section>
-<section class="">Conteudo 2</section>
-<section class="">Conteudo 3</section>
-<section class="">Conteudo 4</section>
 
-<?php include 'footer.php'; ?>
+<header id="sli-principal">
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <img src="https://www.4devs.com.br/4devs_gerador_imagem.php?acao=gerar_imagem&txt_largura=1202&txt_altura=603&extensao=png&fundo_r=0.7200387865304947&fundo_g=0.7427978515625&fundo_b=0.7418415883258611&texto_r=0&texto_g=0&texto_b=0&texto=Gerador%20Imagem%20%234Devs&tamanho_fonte=10" alt="...">
+                <div class="carousel-caption">
+                    ...
+                </div>
+            </div>
+            <div class="item">
+                <img src="https://www.4devs.com.br/4devs_gerador_imagem.php?acao=gerar_imagem&txt_largura=1202&txt_altura=603&extensao=png&fundo_r=0.7200387865304947&fundo_g=0.7427978515625&fundo_b=0.7418415883258611&texto_r=0&texto_g=0&texto_b=0&texto=Gerador%20Imagem%20%234Devs&tamanho_fonte=10" alt="...">
+                <div class="carousel-caption">
+                    ...
+                </div>
+            </div>
+            <div class="item">
+                <img src="https://www.4devs.com.br/4devs_gerador_imagem.php?acao=gerar_imagem&txt_largura=1202&txt_altura=603&extensao=png&fundo_r=0.7200387865304947&fundo_g=0.7427978515625&fundo_b=0.7418415883258611&texto_r=0&texto_g=0&texto_b=0&texto=Gerador%20Imagem%20%234Devs&tamanho_fonte=10" alt="...">
+                <div class="carousel-caption">
+                    ...
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+
+
+<main class="container">
+    <section id="sobre" class="sobre">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="titulo">
+                    <div class="titulo-img">
+                        <i class="fa fa-dot-circle-o fa-4x"></i>
+                    </div>
+                    <div class="titulos">
+                        <h2>SOBRE</h2>
+                        <h1>A ARROYO</h1>
+                    </div>
+                </div>
+                <div class="descricao">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                    </p>
+                    <a href="#" class="btn btn-custom">SAIBA MAIS</a>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <img src="http://lorempixel.com/400/200/sports/1/">
+            </div>
+        </div>
+    </section>
+
+    <section id="servicos" class="servicos"><div class="titulo">
+            <div class="titulo-img">
+                <i class="fa fa-dot-circle-o fa-4x"></i>
+            </div>
+            <div class="titulos">
+                <h2>DE OLHO NOS PETS</h2>
+                <h1>GALERIA DE FOTOS</h1>
+            </div>
+        </div>
+    </section>
+    <section id="planos" class="planos">
+        <div class="titulo">
+            <div class="titulo-img">
+                <i class="fa fa-dot-circle-o fa-4x"></i>
+            </div>
+            <div class="titulos">
+                <h2>CONHEÇA</h2>
+                <h1>NOSSOS PLANOS</h1>
+            </div>
+        </div>
+        <div class="descricao">
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            </p>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="titulo">
+                    <div class="titulo-img">
+                        <i class="fa fa-dot-circle-o fa-4x"></i>
+                    </div>
+                    <div class="titulos">
+                        <h2>FALE CONOSCO</h2>
+                        <h1>CONTATO</h1>
+                    </div>
+                </div>
+                <div class="descricao">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                    </p>
+                </div>
+                <a href="#" class="btn btn-custom">SAIBA MAIS</a> 
+
+            </div>
+            <div class="col-md-4">
+                <div class="titulo">
+                    <div class="titulo-img">
+                        <i class="fa fa-dot-circle-o fa-4x"></i>
+                    </div>
+                    <div class="titulos">
+                        <h2>FALE CONOSCO</h2>
+                        <h1>CONTATO</h1>
+                    </div>
+                </div>
+                <div class="descricao">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                    </p>
+                </div>
+                <a href="#" class="btn btn-custom">SAIBA MAIS</a> 
+
+            </div>
+            <div class="col-md-4">
+                <div class="titulo">
+                    <div class="titulo-img">
+                        <i class="fa fa-dot-circle-o fa-4x"></i>
+                    </div>
+                    <div class="titulos">
+                        <h2>FALE CONOSCO</h2>
+                        <h1>CONTATO</h1>
+                    </div>
+                </div>
+                <div class="descricao">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                    </p>
+                </div>
+                <a href="#" class="btn btn-custom">SAIBA MAIS</a> 
+
+            </div>
+        </div>
+    </section>
+    <section id="contato" class="contato">
+        <div class="titulo">
+            <div class="titulo-img">
+                <i class="fa fa-dot-circle-o fa-4x"></i>
+            </div>
+            <div class="titulos">
+                <h2>FALE CONOSCO</h2>
+                <h1>CONTATO</h1>
+            </div>
+        </div>
+        <div class="descricao">
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            </p>
+        </div>
+        <div class="mapa"></div>
+        <div class="formulario-contato">
+
+        </div>
+    </section>
+
+    <?php include 'footer.php'; ?>
