071ac891874ac0ff4610a2d094a2a1bcb200d2c7
cristiianefaria <cristiane.trc@gmail.com>
2017-12-15 19:25:41 -0200

    ajustando slider, contato e inserindo imagem sobre

--- index.php
+++ index.php
@@ -9,38 +9,48 @@
         <ol class="carousel-indicators">
             <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
             <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
         </ol>
 
         <!-- Wrapper for slides -->
         <div class="carousel-inner" role="listbox">
             <div class="item active">
-                <img src="img/slide-creche.jpg" alt="crech-daycare">
+                <img class="img-responsive" src="img/slide-creche.jpg" alt="crech-daycare">
                 <div class="carousel-caption">
                     <h1>CRECHE DAYCARE</h1>
                     <p>O serviço de Daycare dá ao seu melhor amigo um dia de diversão com muito conforto e
-                    equilíbrio. As atividades são programadas considerando o nível de energia e o porte físico dos
-                    cães que recebem cuidados específicos na alimentação, atividade física e descanso.</p>
+                        equilíbrio. As atividades são programadas considerando o nível de energia e o porte físico dos
+                        cães que recebem cuidados específicos na alimentação, atividade física e descanso.</p>
                 </div>
             </div>
             <div class="item">
-                <img src="img/slide-baia.jpg" alt="hotel sem gaiolas ou baias">
+                <img class="img-responsive" src="img/slide-baia.jpg" alt="hotel sem gaiolas ou baias">
                 <div class="carousel-caption">
                     <h1>HOTEL SEM GAIOLAS OU BAIAS</h1>
                     <p>Enquanto o dono viaja, termina a reforma da casa, ou tem qualquer outro motivo que o
-                    impeça de ficar com seu pet e não quer que ele fique muito tempo em espaços pequenos, o
-                    hotel Arroyo recebe os clientes de quatro patas em um ambiente 100% livre, sem gaiolas ou
-                    baias, dando carinho e atenção até que ele possa voltar para casa.</p>
+                        impeça de ficar com seu pet e não quer que ele fique muito tempo em espaços pequenos, o
+                        hotel Arroyo recebe os clientes de quatro patas em um ambiente 100% livre, sem gaiolas ou
+                        baias, dando carinho e atenção até que ele possa voltar para casa.</p>
                 </div>
             </div>
             <div class="item">
-                <img src="img/slide-natacao.jpg" alt="natação">
+                <img class="img-responsive" src="img/slide-natacao.jpg" alt="natação">
                 <div class="carousel-caption">
                     <h1>NATAÇÃO</h1>
                     <p>Em uma big piscina de 35m monitores e veterinários desenvolvem atividades recreativas, de
-                    fisioterapia, para perda dos quilinhos extras ou só para se refrescar mesmo num dia de muito
-                    calor e baixa humidade.</p>
+                        fisioterapia, para perda dos quilinhos extras ou só para se refrescar mesmo num dia de muito
+                        calor e baixa humidade.</p>
                 </div>
             </div>
+            <!-- Controls -->
+            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
     </div>
 </section>
@@ -70,7 +80,7 @@
                 </div>
             </div>
             <div class="col-md-6">
-                <img class="img-responsive" src="http://lorempixel.com/606/370/sports/1/">
+                <img class="img-responsive" src="img/sobre-home.jpg" width="100%">
             </div>
         </div>
     </div>
@@ -89,31 +99,40 @@
         <!-- Wrapper for slides -->
         <div class="carousel-inner" role="listbox">
             <div class="item active">
-                <img src="img/slide-training.jpg" alt="training básico">
+                <img class="img-responsive" src="img/slide-training.jpg" alt="training básico">
                 <div class="carousel-caption">
                     <h1>TRAINING BÁSICO</h1>
                     <p>Despertando nos cães o desejo de aprender, o método de adestramento inteligente é 
-                    fundamentado em estímulos positivos e recompensas.</p>
+                        fundamentado em estímulos positivos e recompensas.</p>
                 </div>
             </div>
             <div class="item">
-                <img src="img/slide-taxidog.jpg" alt="taxidog">
+                <img class="img-responsive" src="img/slide-taxidog.jpg" alt="taxidog">
                 <div class="carousel-caption">
                     <h1>TAXIDOG</h1>
                     <p>Levamos e buscamos seu amigo de quatro patas na ida e volta para casa com toda segurança e 
-                    o conforto de um veículo com ar condicionado e todo preparado para o transporte deles.</p>
+                        o conforto de um veículo com ar condicionado e todo preparado para o transporte deles.</p>
                 </div>
             </div>
             <div class="item">
-                <img src="img/slide-monitoramento.jpg" alt="monitoramento online">
+                <img class="img-responsive" src="img/slide-monitoramento.jpg" alt="monitoramento online">
                 <div class="carousel-caption">
                     <h1>MONITORAMENTO ONLINE</h1>
                     <p>Os donos de pets hospedados em regime de creche ou hotel contam com o conforto
-                    acompanhar as rotinas online 24h com câmeras que mostram toda a área externa e até o
-                    ambiente reservado para o descanso onde o piso é antiderrapante e a cobertura com
-                    tratamento antirruído.</p>
+                        acompanhar as rotinas online 24h com câmeras que mostram toda a área externa e até o
+                        ambiente reservado para o descanso onde o piso é antiderrapante e a cobertura com
+                        tratamento antirruído.</p>
                 </div>
             </div>
+            <!-- Controls -->
+            <a class="left carousel-control" href="#banners" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#banners" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
     </div>
 </section>
@@ -229,7 +248,7 @@
             </p>
         </div>
         <div class="mapa">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3821.1715451080404!2d-49.2720662!3d-16.7182903!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935ef0d852052f0f%3A0xb54399790432f7ab!2sArroyo+Pets!5e0!3m2!1spt-BR!2sbr!4v1512645514682" width="100%" height="550" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3821.1715451080404!2d-49.2720662!3d-16.7182903!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935ef0d852052f0f%3A0xb54399790432f7ab!2sArroyo+Pets!5e0!3m2!1spt-BR!2sbr!4v1512645514682" width="100%" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
         </div>
         <div class="formulario-contato">
             <div class="row">
@@ -270,14 +289,24 @@
 
                 <div class = "col-md-4 col-xs-12">
                     <div class = "informacoes-contato">
-
-                        <img src="img/icon-local.png">
-                        <p>Av. C_264, 177 - Setor NOva Suiça.</p>
-                        <p>Goiânia - GO. 74280-220</p>
-
-                        <img src="img/icon-telefone.png">
-                        <p>(62) 4004-2017</p>
-                        <p>(62) 98223-9339</p>
+                        <div class = "no-padding col-xs-4">
+                            <img class="img-responsive" src="img/icon-local.png">
+                        </div>
+                        <div class="no-padding col-xs-8">
+                            <p>Av. C_264, 177 - Setor Nova Suiça.</p>
+                            <p>Goiânia - GO. 74280-220</p>
+                        </div>
+                        <div class="no-padding col-xs-4">
+                            <img class="img-responsive" src="img/icon-telefone.png">
+                        </div>
+                        <div class = "no-padding col-xs-8">
+                            <p>(62) 4004-2017</p>
+                            <p>(62) 98223-9339</p>
+                        </div>
+                        <div class="redes-sociais col-xs-12">
+                            <a href="#"><img src="img/FB.png"></a>
+                            <a href="#"><img src="img/ig.png"></a>
+                        </div>
                     </div>
                 </div>
             </div>
