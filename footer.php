90c23b1070d6015a20f7b7b9bdc396f7894355e0
cristiianefaria <cristiane.trc@gmail.com>
2017-12-29 12:05:54 -0200

    inserindo hidemenu

--- footer.php
+++ footer.php
@@ -15,6 +15,8 @@
 <script src="js/googlemaps.js"></script>
 <!-- js modal galeria de imagens -->
 <script src="js/modalGaleria.js"></script>
+<!-- js ocultar menu responsivo -->
+<script src="js/hide-menu.js"></script>
 <!-- chamada do google maps -->
  <script async defer
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyATxZ-Dmg2slQlmM5UIOUreJXXgRTbfKao&callback=initMap">
